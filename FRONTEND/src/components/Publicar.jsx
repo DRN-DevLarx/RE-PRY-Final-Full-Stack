af6f8e4072a6fd106fd15a0b8c5a10363567a096
23dereek <jacquelinemoraaguilar6@gmail.com>
2025-05-29 13:38:19 -0600

    pagina de Publicar

--- FRONTEND/src/components/Publicar.jsx
+++ FRONTEND/src/components/Publicar.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/public.css'; // Asegúrate de tener este archivo CSS
 
+import InteresesServices from '../services/interesesServices';
+
+
 function Publicar() {
+
+  const [Intereses, setIntereses] = useState([])
+  const [ErrorIntereses, setErrorIntereses] = useState([]
+
+  )
+   useEffect(() => {
+        let isMounted = true;
+        const fetch = async () => {
+            try {
+                const DatosIntereses = await InteresesServices.GetIntereses();
+  
+                if (isMounted) {
+                    setIntereses(DatosIntereses);
+                }
+            } catch (error) {
+                if (isMounted) {
+                    setErrorIntereses(error.message);
+                }
+            }
+        };
+    
+        fetch();
+    
+        return () => {
+            isMounted = false;
+        };
+  
+        
+    }, []);
+
+
+
   return (
     <div className="publicarContainer">
       <div className="cuadro">
@@ -22,8 +57,14 @@ function Publicar() {
 
       <div className="cuadro">
 
-        <h3>Categoría</h3>
-        <input type="text" placeholder="Ej: Tecnología" />
+         <select name="" id="">
+              <option value="">Área de trabajo</option>
+            {Intereses.map((interes, index) => (
+                <option key={index} value={interes.nombre_interes}>
+                  {interes.nombre_interes}
+                  </option>
+            ))}
+              </select>
 
         <h3>Salario</h3>
         <input type="text" placeholder="Ej: $150000" />
@@ -31,7 +72,7 @@ function Publicar() {
         <h3>Descripción general y requisitos</h3>
         <textarea placeholder="Describe el puesto..." rows="5"></textarea>
 
-        <div style={{ marginTop: '20px' }}>
+        <div className='btnpublic' style={{ marginTop: '20px' }}>
           <button>Publicar</button>
         </div>
       </div>
