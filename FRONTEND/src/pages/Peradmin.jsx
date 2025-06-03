803d7720c52f9fd96c2ca1dcc897c19e0cd0669e
23dereek <jacquelinemoraaguilar6@gmail.com>
2025-06-03 15:46:49 -0600

    Realizacion de userregi y ultimas ofertas

---
+++ FRONTEND/src/pages/Peradmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Perfiladmin from "../../src/components/Perfiladmin"
+function Peradmin() {
+  return (
+    <div>
+      <Perfiladmin/>
+    </div>
+  )
+}
+
+export default Peradmin
